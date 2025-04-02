0b9ebe87a7a10b799907827f05f4fbfc23060a3e
JoaoVitorErmacora <joao.caramello@gmail.com>
2025-04-02 15:55:09 -0300

    comita tudo

--- app.js
+++ app.js
@@ -40,7 +40,15 @@ o segundo, são os dados que serão enviados ao cliente (RESULT - 'res') */
 app.get("/", (req, res) => {
   // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/
   // res.send(index);
-  res.render("index");
+  // res.render("index");
+  console.log("GET/index");
+  res.redirect("/cadastro"); //redireciona para a rota cadastro
+});
+
+app.get("/cadastro", (req, res) => {
+  console.log("GET/ cadastro");
+  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/cadastro
+  res.render("cadastro");
 });
 
 app.get("/sobre", (req, res) => {
@@ -53,21 +61,50 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/cadastro", (req, res) => {
-  // Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/cadastro
-  res.send(cadastro);
-});
-
+//POST DO CADASTRO
 app.post("/cadastro", (req, res) => {
   !req.body
     ? console.log(JSON.stringify(req.body))
     : console.log(`Body vazio: ${req.body}`);
-  res.send(cadastro);
 
-  //Colocar aqui as validações e inclusão nio banco de dados do cadastro do usuãrio
-  res.send(
-    `Bem-vindo usuário:${req.body.username}, seu email é ${req.body.email}`
-  );
+  const { username, senha, email, cpf, rg, celular } = req.body;
+
+  //Colocar aqui as validações e inclusão no banco de dados do cadastro do usuãrio
+  //1.Validar usuário
+
+  //2. saber se ele já existe no banco
+  query = "SELECT *FROM users WHERE email=? OR cpf=? OR rg=? OR username=?";
+  db.get(query, [email, cpf, rg, username], (err, row) => {
+    if (err) throw err;
+    console.log(`${JSON.stringify(row)}`);
+
+    if (row) {
+      //A variável 'row' irá retornar os dados do banco de dados, executando através do SQL, variável query
+      res.send("Usuário já cadastrado, refaça o cadastro");
+    } else {
+      //3. se usuário não existe no banco, cadastrar
+      const isertQuery =
+        "INSERT INTO users (username, password, email, celular, cpf, rg) VALUES (?, ?, ?, ?, ?, ?)";
+      db.run(
+        isertQuery,
+        [username, password, email, celular, cpf, rg],
+        (err) => {
+          //Inserir a lógica do INSERT
+          if (err) throw err;
+          res.send("Usuário cadastrado com sucesso!");
+        }
+      );
+    }
+  });
+
+  //   res.send(
+  //     `Bem-vindo usuário:${req.body.username}, seu email é ${req.body.email}`
+  //   );
+});
+
+//Programação de rotas do método GET do HTTP 'app.get()'
+app.get("/sobre", (req, res) => {
+  console.log("GET/sobre");
 });
 
 //app.listen() deve ser o último comando da aplicação (app.js)
